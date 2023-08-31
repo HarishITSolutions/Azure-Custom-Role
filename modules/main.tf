0e11c82b9d481d99df407f022959acfc59781ec6
Kamineni, Sri Sai Mani Bhagath (null, consultant) <SriSaiManiBhagath.Kamineni@ge.com>
2023-08-31 18:31:00 +0000

    Added 5 files to /modules

---
+++ modules/main.tf
@@ -0,0 +1,16 @@
+module "PolicyandInitiativeCreation" {
+  source     = "./modules/policy/"
+  sourceYAML = var.sourcePolicyYAML
+}
+
+locals {
+  mgSource = yamldecode(file("${path.root}/${var.sourceMG-YAML}"))
+  mg       = local.mgSource.managementGroups
+}
+
+module "MG-RA-POL-INI-Assignment" {
+  source       = "./modules/mg/"
+  scope        = local.mg
+  sourceMGYAML = var.sourceMG-YAML
+  depends_on   = [module.PolicyandInitiativeCreation]
+}
