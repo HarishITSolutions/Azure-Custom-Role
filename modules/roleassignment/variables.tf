48c2834486fb18bbe7c9e349f5ea4575d9e52285
Rao, Veerabhadra (GE Vernova, consultant) <Veerabhadra.Rao@ge.com>
2023-10-04 09:06:12 +0000

    Added variables.tf

---
+++ modules/roleassignment/variables.tf
@@ -0,0 +1,13 @@
+variable "scope" {
+  description = "List of management groups"
+  type = list(object({
+    Name                  = string
+    DisplayName           = string
+    ParentManagementGroup = string
+    GroupLevel            = number
+  }))
+}
+
+variable "sourceMGYAML" {
+  type = string
+}
