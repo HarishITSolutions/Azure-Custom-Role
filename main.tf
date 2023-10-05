a11cb0a2b5603b3c7278004dec2571ac2de4536d
Harish Nerella <503356053@ge.com>
2023-10-05 15:20:10 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -9,21 +9,3 @@ module "RoleAssignmentCreation" {
   scope      = local.croles
   sourcecustroleYAML = var.sourcecustrole-YAML
 }
-
-// module "PolicyandInitiativeCreation" {
-//   source     = "./modules/policy/"
-//   sourceYAML = var.sourcePolicyYAML
-// }
-
-// locals {
-//   mgSource = yamldecode(file("${path.root}/${var.sourceMG-YAML}"))
-//   mg       = local.mgSource.managementGroups
-// }
-
-// module "MG-RA-POL-INI-Assignment" {
-//   source       = "./modules/mg/"
-//   scope        = local.mg
-//   sourceMGYAML = var.sourceMG-YAML
-//   rootMG       = var.tenantrootMG
-//   #depends_on   = [module.PolicyandInitiativeCreation]
-// }
