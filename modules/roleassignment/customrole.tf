3397fa9c492bcffd5a510ae2d5ea32d0f51d8d03
Rao, Veerabhadra (GE Vernova, consultant) <Veerabhadra.Rao@ge.com>
2023-11-30 11:21:05 +0000

    Updated customrole.tf

--- modules/roleassignment/customrole.tf
+++ modules/roleassignment/customrole.tf
@@ -9,43 +9,42 @@ resource "azurerm_role_definition" "customrole" {
   name        = each.value.RoleName
   scope       = "/providers/Microsoft.Management/managementGroups/${each.value.scope}"
   description = each.value.Description
-
   permissions {
-    actions     = [
-        "*/read",
-        "Microsoft.Authorization/locks/delete",
-        "Microsoft.Authorization/locks/write",
-        "Microsoft.Authorization/policies/auditIfNotExists/action",
-        "Microsoft.Authorization/roleAssignments/delete",
-        "Microsoft.Authorization/roleAssignments/write",
-        "Microsoft.Compute/disks/beginGetAccess/action",
-        "Microsoft.Compute/disks/delete",
-        "Microsoft.Compute/disks/write",
-        "Microsoft.Compute/snapshots/delete",
-        "Microsoft.Compute/snapshots/write",
-        "Microsoft.Compute/virtualMachines/delete",
-        "Microsoft.Compute/virtualMachines/write",
-        "Microsoft.KeyVault/vaults/write",
-        "Microsoft.Network/applicationSecurityGroups/delete",
-        "Microsoft.Network/applicationSecurityGroups/joinIpConfiguration/action",
-        "Microsoft.Network/applicationSecurityGroups/write",
-        "Microsoft.Network/networkInterfaces/delete",
-        "Microsoft.Network/networkInterfaces/join/action",
-        "Microsoft.Network/networkInterfaces/write",
-        "Microsoft.Network/networkSecurityGroups/delete",
-        "Microsoft.Network/networkSecurityGroups/join/action",
-        "Microsoft.Network/networkSecurityGroups/write",
-        "Microsoft.Network/publicIpAddresses/write",
-        "Microsoft.Network/publicIPAddresses/join/action",
-        "Microsoft.Network/publicIPAddresses/delete",
-        "Microsoft.Network/virtualNetworks/subnets/join/action",
-        "Microsoft.Resources/subscriptions/resourceGroups/delete",
-        "Microsoft.Resources/subscriptions/resourceGroups/write",
-        "Microsoft.Storage/storageAccounts/listKeys/action",
-        "Microsoft.Compute/virtualMachines/extensions/write"
-    ]
-    not_actions = []
-  }
+    actions     = 
+  //       "*/read",
+  //       "Microsoft.Authorization/locks/delete",
+  //       "Microsoft.Authorization/locks/write",
+  //       "Microsoft.Authorization/policies/auditIfNotExists/action",
+  //       "Microsoft.Authorization/roleAssignments/delete",
+  //       "Microsoft.Authorization/roleAssignments/write",
+  //       "Microsoft.Compute/disks/beginGetAccess/action",
+  //       "Microsoft.Compute/disks/delete",
+  //       "Microsoft.Compute/disks/write",
+  //       "Microsoft.Compute/snapshots/delete",
+  //       "Microsoft.Compute/snapshots/write",
+  //       "Microsoft.Compute/virtualMachines/delete",
+  //       "Microsoft.Compute/virtualMachines/write",
+  //       "Microsoft.KeyVault/vaults/write",
+  //       "Microsoft.Network/applicationSecurityGroups/delete",
+  //       "Microsoft.Network/applicationSecurityGroups/joinIpConfiguration/action",
+  //       "Microsoft.Network/applicationSecurityGroups/write",
+  //       "Microsoft.Network/networkInterfaces/delete",
+  //       "Microsoft.Network/networkInterfaces/join/action",
+  //       "Microsoft.Network/networkInterfaces/write",
+  //       "Microsoft.Network/networkSecurityGroups/delete",
+  //       "Microsoft.Network/networkSecurityGroups/join/action",
+  //       "Microsoft.Network/networkSecurityGroups/write",
+  //       "Microsoft.Network/publicIpAddresses/write",
+  //       "Microsoft.Network/publicIPAddresses/join/action",
+  //       "Microsoft.Network/publicIPAddresses/delete",
+  //       "Microsoft.Network/virtualNetworks/subnets/join/action",
+  //       "Microsoft.Resources/subscriptions/resourceGroups/delete",
+  //       "Microsoft.Resources/subscriptions/resourceGroups/write",
+  //       "Microsoft.Storage/storageAccounts/listKeys/action",
+  //       "Microsoft.Compute/virtualMachines/extensions/write"
+  //   ]
+  //   not_actions = []
+  // }
 
   assignable_scopes = []
 }
