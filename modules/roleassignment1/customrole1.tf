e27eee48cdb703331196f9c2c64f2d03196ee903
Harish Nerella <503356053@ge.com>
2023-11-30 09:25:36 +0000

    Added customrole1.tf

---
+++ modules/roleassignment1/customrole1.tf
@@ -0,0 +1,22 @@
+locals {
+  croles_map = { for croles in var.input : croles.RoleName => croles }
+}
+
+
+resource "azurerm_role_definition" "customrole" {
+  for_each = { for croles in local.croles_map : croles.RoleName => croles }
+
+  name        = each.value.RoleName
+  scope       = "/providers/Microsoft.Management/managementGroups/${each.value.scope}"
+  description = each.value.Description
+
+  permissions {
+    actions     = [
+        "Microsoft.Management/managementGroups/read",
+        "Microsoft.Management/managementGroups/ subscriptions/read",
+    ]
+    not_actions = []
+  }
+
+  assignable_scopes = []
+}
