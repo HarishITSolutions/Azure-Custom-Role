794e35b108fd24834c00b84d099f820ce0695321
Harish Nerella <503356053@ge.com>
2023-12-01 15:07:25 +0000

    Updated customrole.tf

--- modules/roleassignment/customrole.tf
+++ modules/roleassignment/customrole.tf
@@ -1,16 +1,17 @@
 locals {
-  roles = yamldecode(file("${path.root}/${var.sourcecustrole-YAML}")).custom_roles
+  croles_map = { for croles in var.input : croles.RoleName => croles }
 }
 
-resource "azurerm_role_definition" "custom_roles" {
-  for_each = { for role in local.roles : role.name => role }
 
-  name        = each.value.name
+resource "azurerm_role_definition" "customrole" {
+  for_each = { for croles in local.croles_map : croles.RoleName => croles }
+
+  name        = each.value.RoleName
   scope       = "/providers/Microsoft.Management/managementGroups/${each.value.scope}"
   description = each.value.Description
 
   permissions {
-    actions = each.value.actions
+    actions     = each.value.actions
     not_actions = []
   }
 
