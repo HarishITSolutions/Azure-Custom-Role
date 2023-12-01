183bf4bf688c7c0976a69d45be2d586ae7dd169a
Harish Nerella <503356053@ge.com>
2023-12-01 15:13:11 +0000

    Updated main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 locals {
-  roles = yamldecode(file("${path.root}/${var.sourcecustrole-YAML}")).custom_roles
+  roleSource = yamldecode(file("${path.root}/${var.sourcecustrole-YAML}"))
+  croles     = local.roleSource.CustomRoles
 }
 
 module "RoleAssignmentCreation" {
   source     = "./modules/roleassignment/"
+  input      = local.croles
   sourcecustroleYAML = var.sourcecustrole-YAML
 }
