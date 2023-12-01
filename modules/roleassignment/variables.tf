35a33e12a8c0e77d2f99615acb4c9167d6849ec7
Harish Nerella <503356053@ge.com>
2023-12-01 15:08:10 +0000

    Updated variables.tf

--- modules/roleassignment/variables.tf
+++ modules/roleassignment/variables.tf
@@ -1,3 +1,12 @@
+variable "input" {
+  description = "List of Custom Roles"
+  type = list(object({
+    scope              = string
+    RoleName           = string
+    Description        = string
+    actions            = string
+  }))
+}
 
 variable "sourcecustroleYAML" {
   type = string
